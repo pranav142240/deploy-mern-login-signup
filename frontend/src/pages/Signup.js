97ee91080e70db89822edfbff0633ddc55eb5627
Pranav Prakash <115605559+pranav142240@users.noreply.github.com>
2024-08-16 00:23:23 +0530

    Update Signup.js
    vercel url

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `https://deploy-mern-login-signup-api.vercel.app`;
+            const url = `https://deploy-mern-login-signup-api.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
